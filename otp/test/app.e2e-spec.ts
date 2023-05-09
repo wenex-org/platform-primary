7ebdd168a0ed85765c16c02f47b70988c2d59739
Vahid V <vhid.vz@gmail.com>
2023-05-09 11:45:05 +0330

    update

--- otp/test/app.e2e-spec.ts
+++ otp/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { OtpModule } from './../src/otp.module';
+import { OtpModule } from '../src/otp.module';
 
 describe('OtpController (e2e)', () => {
   let app: INestApplication;
